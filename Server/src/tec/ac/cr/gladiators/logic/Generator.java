de9741af8451f47b712596a1649e0b33cebf1b00
Jesus <sandoval14yamir@hotmail.com>
2019-04-26 15:39:10 -0600

    Commit inicial

--- Server/src/tec/ac/cr/gladiators/logic/Generator.java
+++ Server/src/tec/ac/cr/gladiators/logic/Generator.java
@@ -15,7 +15,7 @@ public class Generator {
     public static ArrayList<Towers> generateTowers(int num){
         ArrayList<Towers> towersArrayList = new ArrayList<>();
         for (int i = 1; i < (num + 1); i++){
-            towersArrayList.add(new Towers(i, i, i));
+            towersArrayList.add(new Towers(i,i, i, i, i));
         }
         return towersArrayList;
     }
--- Server/src/tec/ac/cr/gladiators/logic/Towers.java
+++ Server/src/tec/ac/cr/gladiators/logic/Towers.java
@@ -1,40 +1,121 @@
 package tec.ac.cr.gladiators.logic;
 
+import java.util.Random;
+
 public class Towers {
 
-    public Towers(int typeOfTheAmmo, int damageOfTheAmmo, int rangeOfTheAmmo){
+    Towers(int identification, int typeOfTheAmmo, int damageOfTheAmmo, int rangeOfTheAmmo, int deaths) {
+        this.identification = identification;
         this.typeOfAmmo = typeOfTheAmmo;
         this.damageOutput = damageOfTheAmmo;
         this.range = rangeOfTheAmmo;
+        this.deaths = deaths;
     }
 
-    public Towers(){}
+    public static Towers getTower(int identification) {
+            Random rn = new Random();
+            Towers tower = new Builder().setIdentification(identification)
+                    .typeOfAmmo(rn.nextInt(3))
+                    .damageOutput(rn.nextInt(100))
+                    .range(rn.nextInt(5)).build();
+            tower.fillGenes();
+            return tower;
+        }
 
+    private void fillGenes() {
+        int[] genes = new int[3];
+        genes[0] = this.getTypeOfAmmo();
+        genes[1] = this.getDamageOutput();
+        genes[2] = this.getRange();
+        this.setGenes(genes);
+    }
+
+
+    private int identification;
     private int typeOfAmmo;
     private int damageOutput;
     private int range;
+    private int deaths;
+    private int[] genes = new int[3];
 
-    public int getTypeOfAmmo() {
+    public int getIdentification () {
+        return identification;
+    }
+
+    public void setIdentification(int identification){
+        this.identification = identification;
+    }
+
+    public int getTypeOfAmmo () {
         return typeOfAmmo;
     }
 
-    public void setTypeOfAmmo(int typeOfAmmo) {
+    public void setTypeOfAmmo ( int typeOfAmmo){
         this.typeOfAmmo = typeOfAmmo;
     }
 
-    public int getDamageOutput() {
+    public int getDamageOutput () {
         return damageOutput;
     }
 
-    public void setDamageOutput(int damageOutput) {
+    public void setDamageOutput ( int damageOutput){
         this.damageOutput = damageOutput;
     }
 
-    public int getRange() {
+    public int getRange () {
         return range;
     }
 
-    public void setRange(int range) {
+    public void setRange ( int range){
         this.range = range;
     }
-}
+
+    public int[] getGenes () {
+        return genes;
+    }
+
+    public void setGenes ( int[] genes){
+        this.genes = genes;
+    }
+
+    public static class Builder {
+        private int identification;
+        private int typeOfAmmo;
+        private int damageOutput;
+        private int range;
+        private int deaths;
+
+
+        public Builder() {}
+
+        public Builder typeOfAmmo(int typeOfAmmo) {
+            this.typeOfAmmo = typeOfAmmo;
+            return this;
+        }
+
+        public Builder damageOutput(int damageOutput) {
+            this.damageOutput = damageOutput;
+            return this;
+        }
+
+        public Builder range(int range) {
+            this.range = range;
+            return this;
+        }
+
+        public Builder deaths(int deaths) {
+            this.deaths = deaths;
+            return this;
+        }
+        public Builder setIdentification(int identification){
+            this.identification = identification;
+            return this;
+        }
+
+
+        public Towers build() {
+            return new Towers(identification, typeOfAmmo, damageOutput, range, deaths);
+        }
+    }
+
+    }
